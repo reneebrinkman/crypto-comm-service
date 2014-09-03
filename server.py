850bf5d164eb3672c879c9fce61dd507ef0eca7f
Benjamin Brinkman <ben@benjaminbrinkman.com>
2014-09-03 10:11:00 -0400

    server module that loads the routines in the protocol module

---
+++ server.py
@@ -0,0 +1,15 @@
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import protocol
+
+# Restrict to a particular path.
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+# Create server
+server = SimpleXMLRPCServer(("localhost", 8000),
+                            requestHandler=RequestHandler)
+server.register_introspection_functions()
+
+server.register_instance(protocol.CryptoProtocol())
+server.serve_forever()
